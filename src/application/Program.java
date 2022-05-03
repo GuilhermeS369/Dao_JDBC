6eba4dbeed40cdb4c7ed24d60d30eb9ee33490a3
Guilherme Santos <guilherme460@yahoo.com.br>
2022-05-03 19:17:52 -0300

    Criação do Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,20 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
 		
-
+		Department obj = new Department(1, "Brinquedos");
+		System.out.println(obj);
+		Seller sell = new Seller(1, "Luiz", "guilherme460@yahoo.com.br", sdf1.parse("22/06/1999"), 2000.0, obj );
+		System.out.println(sell);
 	}
 
 }
