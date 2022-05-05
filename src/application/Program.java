cda3ae35838ab9dc876c9fff00bf5236510083ee
Guilherme Santos <guilherme460@yahoo.com.br>
2022-05-05 08:40:23 -0300

    Implementação do UPDATE

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,13 +34,16 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n === TEST 4: Insert ====");
+		System.out.println("\n === TEST 4: INSERT ====");
 		Seller seller2 = new Seller(null,"Luiz", "teste@teste.com", new Date(), 9999.99, department );
 		sellerDao.insert(seller2);
-				System.out.println("Inserido! O novo id = " + seller2.getId());
-				
-				
-		
+			System.out.println("Inserido! O novo id = " + seller2.getId());
+			
+		System.out.println("\n === TEST 5: UPDATE ====");	
+		Seller seller3 = sellerDao.findById(1);
+		seller3.setName("Bruce Wayne");
+		sellerDao.update(seller3);
+		System.out.println("Update completed");
 	}
 
 }
--- src/model/dao/imlp/SellerDaoJDBC.java
+++ src/model/dao/imlp/SellerDaoJDBC.java
@@ -73,7 +73,36 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null; //RECEBE O COMANDO
+		
+		try { //PREPARA A QUERY COM RETORNO DE UM NUMERO
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+"SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+"WHERE Id = ?", 
+					Statement.RETURN_GENERATED_KEYS);
+			Department dep = obj.getDepartment();	
+			// CHAMA O DEPARTAMENTO DO OBJETO
+			// A DATA AQUI PRECISA SER DO JAVA.SQL
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));	
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, dep.getId());
+			
+			st.setInt(6, obj.getId());
+			
+			st.executeUpdate();
+			
+			
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			
+		}
 		
 	}
 
